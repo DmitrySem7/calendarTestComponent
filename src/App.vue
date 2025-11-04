d2cb88cbb55c22e4360d4c1c2069820f11adfc4f
Dmitry Sem <dmitrySem7@yandex.ru>
2025-11-04 19:52:39 +0300

    Init-commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CalendarTestComponent from './components/CalendarTestComponent.vue'
+</script>
+
+<template>
+  <div>
+    <CalendarTestComponent />
+  </div>
+</template>
+
